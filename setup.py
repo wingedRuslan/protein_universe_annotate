03b44392423a95acbdd3223558594c1974f06f29
Ruslan Yermakov <yermakovruslan@gmail.com>
2023-04-27 17:36:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from setuptools import setup, find_packages
 
 # Specify the required dependencies for the package
 install_requires = [
+    'scipy==1.7.0',
+    'numpy==1.22.4',
+    'pandas==1.5.3',
+    'pytest==7.2.2',
+    'seaborn==0.12.2',
+    'scikit-learn==1.0.2.',
+    'tokenizers==0.13.3',
+    'tqdm==4.65.0',
+    'transformers==4.28.1',
+    'tensorflow==2.12.0',
     'numpy==1.21.2',
-    'pandas==1.3.2',
+    'pandas==1.3.2'
 ]
 
 setup(
